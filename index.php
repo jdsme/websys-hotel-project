7c5a1b6ec4c818595e0d959f5a88da8f2a4ea8c8
Jemle Serrano <delacruzjemle9@gmail.com>
2026-01-17 20:03:26 +0800

    minor adjustments

--- index.php
+++ index.php
@@ -13,51 +13,61 @@
 
 
     <!-- NAVIGATION BAR -->
-<img src="images/beachview.jpg" class="img-fluid hero-image" alt="beachview">
+    <nav id="navbar-example2" class="navbar bg-body-tertiary px-3 mb-3">
+        <a class="navbar-brand" href="#">The Grand Aurora</a>
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link" href="#scrollspyHeading1">ACCOMODATIONS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#scrollspyHeading2">RATING</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#scrollspyHeading3">GALLERY</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#scrollspyHeading4">DINING</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#scrollspyHeading5">ABOUT US</a>
+            </li>
+        </ul>
+    </nav>
 
-        <nav id="navbar-example2" class="navbar bg-body-tertiary px-3 mb-3">
-  <a class="navbar-brand" href="#">The Grand Aurora</a>
-  <ul class="nav nav-pills">
-    <li class="nav-item">
-      <a class="nav-link" href="#scrollspyHeading1">ACCOMODATIONS</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#scrollspyHeading2">RATING</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#scrollspyHeading3">GALLERY</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#scrollspyHeading4">DINING</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#scrollspyHeading5">ABOUT US</a>
-    </li>
-  </ul>
-</nav>
+    <!-- Hero Image -->
+    <img src="images/beachview.jpg" class="img-fluid hero-image" alt="beachview">
 
-<div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="scrollspy-example bg-body-tertiary p-3 rounded-2" tabindex="0">
-  <h4 id="scrollspyHeading1">Accomodations</h4>
-  <p>Our Accomodations consists of...</p>
+    <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="scrollspy-example bg-body-tertiary p-3 rounded-2" tabindex="0">
+        <h4 id="scrollspyHeading1">Accomodations</h4>
+        <p>Our Accomodations consists of...</p>
 
-    <h4 id="scrollspyHeading2">Rating</h4>
-    <p>...</p>
-
-    <h4 id="scrollspyHeading3">Gallery</h4>
-    <p>...</p>
-
-    <h4 id="scrollspyHeading4">Dining </h4>
+        <h4 id="scrollspyHeading2">Rating</h4>
         <p>...</p>
 
-    <h4 id="scrollspyHeading5">About Us</h4>
-    <p>...</p>
-</div>
+        <h4 id="scrollspyHeading3">Gallery</h4>
+        <p>...</p>
+
+        <h4 id="scrollspyHeading4">Dining </h4>
+        <p>...</p>
+
+        <h4 id="scrollspyHeading5">About Us</h4>
+        <p>...</p>
+    </div>
 
 
     <!-- Welcome Page -->
     <div class="welcome-section">
-    <img src="images/outsideview.jpg" class="img-fluid" alt="outsideview">
-    <h3>Welcome to your luxurious home away from home</h3>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <h3>Welcome to your luxurious home away from home</h3>
+                    <p>Experience unparalleled luxury and comfort at The Grand Aurora Hotel. Our world-class amenities and exceptional service ensure your stay is nothing short of extraordinary. From breathtaking ocean views to elegant accommodations, every detail is designed with your comfort in mind.</p>
+                </div>
+                <div class="col-md-6">
+                    <img src="images/outsideview.jpg" class="img-fluid" alt="outsideview">
+                </div>
+            </div>
+        </div>
     </div>
 
 
@@ -66,15 +76,17 @@
     <div class="accomodations-section">
         <h2>ACCOMODATIONS</h2>
         
-        <div class="row g-3">
-            <div class="col-md-4">
-                <img src="images/room1.jpg" class="img-fluid accommodation-img" alt="Room 1">
-            </div>
-            <div class="col-md-4">
-                <img src="images/room2.jpg" class="img-fluid accommodation-img" alt="Room 2">
-            </div>
-            <div class="col-md-4">
-                <img src="images/room3.jpg" class="img-fluid accommodation-img" alt="Room 3">
+        <div class="container">
+            <div class="row g-3">
+                <div class="col-md-4">
+                    <img src="images/room1.jpg" class="img-fluid accommodation-img" alt="Room 1">
+                </div>
+                <div class="col-md-4">
+                    <img src="images/room2.jpg" class="img-fluid accommodation-img" alt="Room 2">
+                </div>
+                <div class="col-md-4">
+                    <img src="images/room3.jpg" class="img-fluid accommodation-img" alt="Room 3">
+                </div>
             </div>
         </div>
     </div>
