66a6552b303b60c9328fd681227384470df9090b
Jemle Serrano <delacruzjemle9@gmail.com>
2026-01-17 19:46:19 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Grand Aurora Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles.css">
+    
+</head>
+<body>
+
+
+    <!-- NAVIGATION BAR -->
+<img src="images/beachview.jpg" class="img-fluid" alt="beachview">
+
+        <nav id="navbar-example2" class="navbar bg-body-tertiary px-3 mb-3">
+  <a class="navbar-brand" href="#">The Grand Aurora</a>
+  <ul class="nav nav-pills">
+    <li class="nav-item">
+      <a class="nav-link" href="#scrollspyHeading1">ACCOMODATIONS</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#scrollspyHeading2">RATING</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#scrollspyHeading3">GALLERY</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#scrollspyHeading3">DINING</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#scrollspyHeading3">ABOUT US</a>
+    </li>
+  </ul>
+</nav>
+
+<div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="scrollspy-example bg-body-tertiary p-3 rounded-2" tabindex="0">
+  <h4 id="scrollspyHeading1">Accomodations</h4>
+  <p>Our Accomodations consists of...</p>
+
+    <h4 id="scrollspyHeading2">Rating</h4>
+    <p>...</p>
+
+    <h4 id="scrollspyHeading3">Gallery</h4>
+    <p>...</p>
+
+    <h4 id="scrollspyHeading4">Dining </h4>
+        <p>...</p>
+
+    <h4 id="scrollspyHeading5">About Us</h4>
+    <p>...</p>
+</div>
+
+
+    <img src="..." class="img-thumbnail" alt="...">
+
+
+    <!-- Welcome Page -->
+
+    
+
+<footer></footer>
+</body>
+</html>
